b578341cbb085e8eea1708c0f2c6bfef86072801
Enrique Martel <rock.enrique@gmail.com>
2025-07-19 06:02:20 +0000

    FirebaseError: Firebase: Error (auth/api-key-not-valid.-please-pass-a-va

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCWJ97gYG4fkUB0IbBXYl4dHStCg_yOTGw",
+  authDomain: "stok-io.firebaseapp.com",
+  projectId: "stok-io",
+  storageBucket: "stok-io.appspot.com",
+  messagingSenderId: "226589234695",
+  appId: "1:226589234695:web:f7879c220bddd3c195fca2",
+  measurementId: "G-5NVZSN17E6"
 };
 
 // Initialize Firebase

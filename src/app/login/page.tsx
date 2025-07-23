dcfd60011ed40035d3eed3a80f70205183c74f3c
Enrique Martel <enrique@martel.page>
2025-07-23 17:01:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -101,10 +101,8 @@ export default function LoginPage() {
             <div className="grid gap-2">
               <div className="flex items-center">
                 <Label htmlFor="password">Senha</Label>
-                 <Link href="/forgot-password" passHref legacyBehavior>
-                    <a className="ml-auto inline-block text-sm underline">
-                        Esqueceu sua senha?
-                    </a>
+                 <Link href="/forgot-password" className="ml-auto inline-block text-sm underline">
+                    Esqueceu sua senha?
                 </Link>
               </div>
               <div className="relative">
